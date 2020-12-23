cc44a30fb9f5137cd321f10ef2731c7f93749cb6
kadobkina <kadobkina.ea@mail.ru>
2020-12-23 23:17:08 +0300

    add_comments

--- Program.cs
+++ Program.cs
@@ -12,12 +12,13 @@ namespace ta13
     {
  
         const string path = "grammar.txt";
-        static string[] lines = new string[count_of_row(path)]; // строки грамматики из файла
-        static string[] nonterm = new string[count_of_row(path)]; // список нетерминалов
-        static string[] chain = new string[count_of_row(path)]; // список цепей
-        static string[] result_grammar = new string[count_of_row(path)]; //грамматика без цепных продукций
+        static int count_gr = count_of_row(path);
+        static string[] lines = new string[count_gr]; // грамматика с цепными продукциями из файла
+        static string[] nonterm = new string[count_gr]; // список нетерминалов
+        static string[] chain = new string[count_gr]; // список цепных продукций
+        static string[] result_grammar = new string[count_gr]; //грамматика без цепных продукций
 
-
+        // количество строк в грамматике
         public static int count_of_row(string p)
         {
             int count = 0;
@@ -32,11 +33,11 @@ namespace ta13
             return count;
         }
 
+        // рекурсивный поиск цепных продукций
         public static void add_chain(int i)
         {
             string line = lines[i];
             string[] split_line = line.Split(' ');
-            //result_grammar[i] = split_line[0] + " ";
             foreach (string c in split_line)
             {
                 if (nonterm.Contains(c))
@@ -72,7 +73,7 @@ namespace ta13
 
             WriteLine("Исходная грамматика в файле ..bin/Debug/grammar.txt  >>>");
 
-            // считываем файл построчно
+            // считываем файл построчно, выписываем нетерминалы
             try
             {
                 using (StreamReader sr = new StreamReader(path))
@@ -93,34 +94,14 @@ namespace ta13
                 WriteLine(e.Message);
             }
 
-            WriteLine("Грамматика после удаления цепных продукций  >>>");
 
+            // находим цепные продукции, начиная с начальной
             add_chain(0);
 
             /*            foreach (var c in chain)      // проверка циклов
                             WriteLine(c);*/
 
-
-          /*    for (int i = 0; i < chain.GetLength(0); i++)
-            {
-                for (int j = 1; j < chain.GetLength(1); j++)
-                {
-                    string el = chain[i, j];
-                    foreach (var gram in result_grammar)
-                    {
-                        if ((gram[0] + "").CompareTo(el) == 0)
-                        {
-                            var gr = gram.Split(' ');
-                            for (int k = 1; k < gr.Length; k++)
-                            {
-                                result_grammar[i] += gr[k] + " ";
-                            }
-                        }
-                    }
-
-                }
-            }*/
-
+            // удаляем цепные продукции из итоговой грамматики, добавляя варианты переходов
             for (int i = 0; i < chain.Length; i++)
             {
                 var split_chain = chain[i].Split(' ');
@@ -142,6 +123,8 @@ namespace ta13
             }
 
 
+            // вывод итоговой грамматики без цепных продукций
+            WriteLine("\nГрамматика после удаления цепных продукций  >>>");
             foreach (var g in result_grammar)
             {
                 WriteLine(g);
